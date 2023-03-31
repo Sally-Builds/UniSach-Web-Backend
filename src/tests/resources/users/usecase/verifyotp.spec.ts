4ebeefa7fbc1a8b1a83dcc32e7a2f4cbd2446aaa
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-03-31 19:12:28 +0100

    verifyOTP test: refactoring

--- src/tests/resources/users/usecase/verifyotp.spec.ts
+++ src/tests/resources/users/usecase/verifyotp.spec.ts
@@ -1,42 +1,57 @@
 import VerifyUsecase from '../../../../resources/api/users/usecase/auth/verifyOTP.usecase'
-import { UserRepositoryVerifyOTP, UserRepository, JwtGen } from '../../__helpers__/stubs';
+import {JwtGen, dbUser } from '../../__helpers__/stubs';
 import EmailTest from '../../__helpers__/email.stub';
 import Exception from '../../../../utils/exception/Exception';
+import UserRepositoryInterface from '../../../../resources/api/users/interfaces/userRepo.interface';
+
+
+const UserRepositoryFindOneReturnsAValue:UserRepositoryInterface = {
+    createUser: jest.fn().mockReturnValue(Promise.resolve()),
+    getUserByEmail: jest.fn().mockReturnValue(Promise.resolve(null)),
+    findOne: jest.fn().mockReturnValue(Promise.resolve(dbUser()[0])),
+    findOneAndUpdate: jest.fn().mockReturnValue(Promise.resolve(null)),
+}
+const UserRepositoryFindOneReturnsNull:UserRepositoryInterface = {
+    createUser: jest.fn().mockReturnValue(Promise.resolve()),
+    getUserByEmail: jest.fn().mockReturnValue(Promise.resolve(null)),
+    findOne: jest.fn().mockReturnValue(Promise.resolve(null)),
+    findOneAndUpdate: jest.fn().mockReturnValue(Promise.resolve(null)),
+}
 
 describe("Test Verify OTP usecase", () => {
     const EmailClass = new EmailTest()
-    const verifyUsecase = new VerifyUsecase(UserRepositoryVerifyOTP, JwtGen, EmailClass)
-
-    const verifyUsecase2 = new VerifyUsecase(UserRepository, JwtGen, EmailClass)
-
-
     it('should encrypt the otp', async () => {
+        const verifyUsecase = new VerifyUsecase(UserRepositoryFindOneReturnsAValue, JwtGen, EmailClass)
         const encryptOTPSpy = jest.spyOn(verifyUsecase, 'encryptOTP')
-       await verifyUsecase.execute('johndoe@gmail.com', '123456', '')
+        await verifyUsecase.execute('johndoe@gmail.com', '123456', '')
 
         expect(encryptOTPSpy).toHaveBeenCalled()
     })
 
     it('should throw an exception if no user is found', async () => {
-        await expect(async () => {await verifyUsecase2.execute('johndoe@gmail.com', '123456', '')})
+        const verifyUsecase = new VerifyUsecase(UserRepositoryFindOneReturnsNull, JwtGen, EmailClass)
+        await expect(async () => {await verifyUsecase.execute('johndoe@gmail.com', '123456', '')})
         .rejects.toThrow(new Exception("Wrong OTP or Expired OTP", 400))
     })
 
-    it('should call findOneAndUpdate 2 times',async () => {
-        const findOneAndUpdateSpy = jest.spyOn(UserRepositoryVerifyOTP, 'findOneAndUpdate')
-       await verifyUsecase.execute('johndoe@gmail.com', '123456', '')
+    it('should call generateTokens with the correct parameter',async () => {
+        const verifyUsecase = new VerifyUsecase(UserRepositoryFindOneReturnsAValue, JwtGen, EmailClass)
+        const generateTokensSpy = jest.spyOn(verifyUsecase, 'generateTokens')
+        await verifyUsecase.execute('johndoe@gmail.com', '123456', '')
 
-        expect(findOneAndUpdateSpy).toBeCalledTimes(2)
+        expect(generateTokensSpy).toHaveBeenCalledWith('1')
     })
 
     it('should generate the refresh and access tokens',async () => {
+        const verifyUsecase = new VerifyUsecase(UserRepositoryFindOneReturnsAValue, JwtGen, EmailClass)
         const JwtGenSpy = jest.spyOn(JwtGen, 'sign')
-       await verifyUsecase.execute('johndoe@gmail.com', '123456', '')
+        await verifyUsecase.execute('johndoe@gmail.com', '123456', '')
 
         expect(JwtGenSpy).toBeCalledTimes(2)
     })
 
     it('should send welcome email to user', async () => {
+        const verifyUsecase = new VerifyUsecase(UserRepositoryFindOneReturnsAValue, JwtGen, EmailClass)
         const emailSpy = jest.spyOn(EmailClass, 'sendWelcome')
         await verifyUsecase.execute('johndoe@gmail.com', '123456', '')
 
@@ -45,6 +60,7 @@ describe("Test Verify OTP usecase", () => {
 
 
     it('should return valid data', async () => {
+        const verifyUsecase = new VerifyUsecase(UserRepositoryFindOneReturnsAValue, JwtGen, EmailClass)
         const data = await verifyUsecase.execute('johndoe@gmail.com', '123456', '')
 
         expect(data).toEqual(expect.objectContaining({
