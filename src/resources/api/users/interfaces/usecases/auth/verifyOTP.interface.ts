da10094d35ad77fd0ed2e6759aada48168ee7f3f
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-03-31 19:11:57 +0100

    verifyOTP usecase: refactoring

--- src/resources/api/users/interfaces/usecases/auth/verifyOTP.interface.ts
+++ src/resources/api/users/interfaces/usecases/auth/verifyOTP.interface.ts
@@ -3,8 +3,14 @@ import User from "../../user.interface"
 export default interface VerifyOTPInterface {
     execute(email: string, OTP: string, refreshToken: string): Promise<VerifyOTP.Response>
     encryptOTP(OTP: string): string
+    generateTokens(id: string): Promise<GenerateToken.Response>
+    removeUnwantedFields(user: User): void
 }
 
 export namespace VerifyOTP {
     export type Response = {user: User, accessToken: string, refreshToken: string}
+}
+
+export namespace GenerateToken {
+    export type Response = {accessToken: string, newRefreshToken: string}
 }
--- src/resources/api/users/usecase/auth/verifyOTP.usecase.ts
+++ src/resources/api/users/usecase/auth/verifyOTP.usecase.ts
@@ -1,9 +1,10 @@
 import Exception from "@/utils/exception/Exception";
-import VerifyOTPInterface, {VerifyOTP} from "../../interfaces/usecases/auth/verifyOTP.interface";
+import VerifyOTPInterface, {GenerateToken, VerifyOTP} from "../../interfaces/usecases/auth/verifyOTP.interface";
 import crypto from 'crypto'
 import UserRepositoryInterface from "../../interfaces/userRepo.interface";
 import EmailInterface from "../../../email/email.interface";
 import { JwtGenerate } from "../../../../../utils/cryptography/interface/cryptography/jsonwebtoken/generate";
+import User from "../../interfaces/user.interface";
 
 
 export default class VerifyOTPUsecase implements VerifyOTPInterface {
@@ -12,29 +13,22 @@ export default class VerifyOTPUsecase implements VerifyOTPInterface {
 
     async execute(email: string, OTP: string, refreshToken: string): Promise<VerifyOTP.Response> {
         try {
-            const hashedOTP = this.encryptOTP(OTP)
+            const hashOTP = this.encryptOTP(OTP)
 
-            const user = await this.UserRepository.findOne({email, confirmationCodeExpiresIn: {$gt: Date.now()}, verificationCode: hashedOTP})
+            const user = await this.UserRepository.findOne({email, confirmationCodeExpiresIn: {$gt: Date.now()}, verificationCode: hashOTP})
 
             if(!user) throw new Exception("Wrong OTP or Expired OTP", 400)
 
-            await this.UserRepository.findOneAndUpdate({email: user.email}, {emailVerificationStatus: 'active'})
-
-           const newRefreshTokenArray = !refreshToken ? user.refreshToken : user.refreshToken?.filter((rt: string) => rt != refreshToken) 
+            const newRefreshTokenArray = !refreshToken ? user.refreshToken : user.refreshToken?.filter((rt: string) => rt != refreshToken) 
            
-            const accessToken = await this.jwtGen.sign((user as any).id, String(process.env.ACCESS_TOKEN_SECRET), String(process.env.ACCESS_TOKEN_EXPIRES_IN))
-            const newRefreshToken = await this.jwtGen.sign((user as any).id, String(process.env.REFRESH_TOKEN_SECRET), String(process.env.REFRESH_TOKEN_EXPIRES_IN))
+            const {accessToken, newRefreshToken} = await this.generateTokens((user as any).id)
             let res = {user, accessToken, refreshToken: newRefreshToken}
 
             newRefreshTokenArray?.push(newRefreshToken)
-            await this.UserRepository.findOneAndUpdate({_id: (user as any).id}, {refreshToken: newRefreshTokenArray})
+            await this.UserRepository.findOneAndUpdate({_id: (user as any).id}, {refreshToken: newRefreshTokenArray, emailVerificationStatus: 'active'})
             await this.Email.sendWelcome('http://localhost:3000/login', user.email, (user as any).first_name)
 
-            user.password = undefined;
-            user.verificationCode = undefined
-            user.confirmationCodeExpiresIn = undefined
-            user.emailVerificationStatus = 'active'
-            user.refreshToken = undefined
+            this.removeUnwantedFields(user)
 
             return res
         } catch (error:any) {
@@ -44,5 +38,20 @@ export default class VerifyOTPUsecase implements VerifyOTPInterface {
 
     encryptOTP(OTP: string): string {
         return crypto.createHash('sha256').update(OTP).digest('hex')
+    }
+    
+    async generateTokens(id: string): Promise<GenerateToken.Response> {
+        const accessToken = await this.jwtGen.sign(id, String(process.env.ACCESS_TOKEN_SECRET), String(process.env.ACCESS_TOKEN_EXPIRES_IN))
+        const newRefreshToken = await this.jwtGen.sign(id, String(process.env.REFRESH_TOKEN_SECRET), String(process.env.REFRESH_TOKEN_EXPIRES_IN))
+        
+        return {accessToken, newRefreshToken}
+    }
+
+    removeUnwantedFields(user: User) {
+        user.password = undefined;
+        user.verificationCode = undefined
+        user.confirmationCodeExpiresIn = undefined
+        user.emailVerificationStatus = 'active'
+        user.refreshToken = undefined
     }
 }
